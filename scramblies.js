878533282aa287298945bed23996df092925f425
taejin <taejin5314@gmail.com>
2021-05-26 14:54:35 -0400

    Update scramblies.js

--- scramblies.js
+++ scramblies.js
@@ -1,13 +1,27 @@
 function scramble(str1, str2) {
   let temp1 = str1.split(''), temp2 = str2.split('');
-  for (let i = 0; i < temp1.length; i++) {
-    for (let j = 0; j < temp2.length; j++) {
-      if (str1[i] === str2[j]) {
-        temp1[i] = '';
-        temp2[j] = '';
+  for (let i = 0; i < temp2.join().length; i++) {
+    for (let j = 0; j < temp1.join().length; j++) {
+      if (temp1[j] === temp2[i]) {
+        temp2[i] = '';
+        temp1[j] = '';
+        i--;
+        j--;
+        console.log(temp2.join(''), j)
       }
     }
   }
-  if (temp2.length === 0) return true;
+  if (temp2.join('').length === 0) return true;
   return false;
 }
+
+console.log(scramble('rkqodlw', 'world'), true);
+console.log(scramble('cedewaraaossoqqyt', 'codewars'), true);
+console.log(scramble('katas', 'steak'), false);
+console.log(scramble('scriptjava', 'javascript'), true);
+console.log(scramble('scriptingjava', 'javascript'), true);
+console.log(scramble('scriptsjava', 'javascripts'), true);
+console.log(scramble('jscripts', 'javascript'), false);
+console.log(scramble('aabbcamaomsccdd', 'commas'), true);
+console.log(scramble('aaaab', 'aaaaaaaaaaabbbb'))
+console.log(scramble('awegaweawegaweherhaerjerjaerjaerjaerj', 'aweawfaoijozixjvoizxjoivjzxoijvoijoisjfooiaejofiawejoifjoixzjoi'))
