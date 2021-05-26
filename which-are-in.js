f8dbfe7fc8c419b8c04dc1d197c6571d356a66de
taejin <taejin5314@gmail.com>
2021-05-26 16:32:08 -0400

    Update which-are-in.js

--- which-are-in.js
+++ which-are-in.js
@@ -1,11 +1,18 @@
 function inArray(array1, array2) {
+  let arr = [];
+  for (let i = 0; i < array2.length; i++) {
+    for (let j = 0; j < array1.length; j++) {
+      if (array2[i].includes(array1[j])) !arr.includes(array1[j]) && arr.push(array1[j]);
+    }
+  }
+  return arr.sort();
 }
 
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 a1 = ["xyz", "live", "strong"]
-Test.assertSimilar(inArray(a1, a2), ["live", "strong"])
+console.log(inArray(a1, a2), ["live", "strong"])
 a1 = ["live", "strong", "arp"]
-Test.assertSimilar(inArray(a1, a2), ["arp", "live", "strong"])
+console.log(inArray(a1, a2), ["arp", "live", "strong"])
 a1 = ["tarp", "mice", "bull"]
-Test.assertSimilar(inArray(a1, a2), [])
+console.log(inArray(a1, a2), [])
