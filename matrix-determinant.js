1ed4dafa81575df8118ecca9f431177d0610bb9b
taejin <taejin5314@gmail.com>
2021-05-26 18:38:19 -0400

    Create matrix-determinant.js

---
+++ matrix-determinant.js
@@ -0,0 +1,10 @@
+function determinant(m) {
+  // return the determinant of the matrix passed in
+};
+
+m1 = [[1, 3], [2, 5]]
+m2 = [[2, 5, 3], [1, -2, -1], [1, 3, 4]]
+
+console.log(determinant([[1]]), 1)
+console.log(determinant(m1), -1)
+console.log(determinant(m2), -20)
