3f7926b62c1bf3b72a7ee485f1dae5b9878cadca
taejin <taejin5314@gmail.com>
2021-05-26 16:08:33 -0400

    Update the-hashtag-generator.js

--- the-hashtag-generator.js
+++ the-hashtag-generator.js
@@ -1,4 +1,12 @@
 function generateHashtag(str) {
+  let trimmedStr = str.trim();
+  let arr = trimmedStr.split(' ');
+  if (trimmedStr.length === 0) return false;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    if (arr[i].length > 140) return false;
+  }
+  return "#" + arr.join('');
 }
 
 console.log(generateHashtag(""), false, "Expected an empty string to return false")
@@ -7,3 +15,4 @@ console.log(generateHashtag("Codewars"), "#Codewars", "Should handle a single wo
 console.log(generateHashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
 console.log(generateHashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
 console.log(generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
+console.log(generateHashtag('    a    '))
