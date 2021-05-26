d2db7e919ef878303e80a04bbda49d335eeafca4
taejin <taejin5314@gmail.com>
2021-05-26 16:38:24 -0400

    Create eureka.js

---
+++ eureka.js
@@ -0,0 +1,11 @@
+function sumDigPow(a, b) {
+  // Your code here
+}
+
+console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
+console.log(sumDigPow(10, 100), [89])
+console.log(sumDigPow(90, 100), [])
+console.log(sumDigPow(90, 150), [135])
+console.log(sumDigPow(50, 150), [89, 135])
+console.log(sumDigPow(10, 150), [89, 135])
