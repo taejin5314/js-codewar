a106fa40db33acbe21b6d70a2cd8a1bed3fd761f
taejin <taejin5314@gmail.com>
2021-05-26 21:59:28 -0400

    Update matrix-determinant.js

--- matrix-determinant.js
+++ matrix-determinant.js
@@ -1,5 +1,14 @@
 function determinant(m) {
-  // return the determinant of the matrix passed in
+  let result = 0;
+  if (m[0].length === 1) return m[0][0];
+  else if (m[0].length === 2) {
+    return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+  } else if (m[0].length > 2) {
+    for (let i = 0; i < m[0].length; i++) {
+
+      result += m[0][i] * 
+    }
+  }
 };
 
 m1 = [[1, 3], [2, 5]]
