c25c24378b03c4adb0c3932c92721009a3a35206
taejin <taejin5314@gmail.com>
2021-05-26 22:25:48 -0400

    Update parseInt-reloaded.js

--- parseInt-reloaded.js
+++ parseInt-reloaded.js
@@ -1,4 +1,5 @@
 function parseInt(string) {
+  const map = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "sever": 7, "eight": 8, "nine": 9, ""}
 }
 
 console.log(parseInt('one'), 1);
