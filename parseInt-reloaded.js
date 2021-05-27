c1874954459ea290ab613426ab46d185aca62581
taejin <taejin5314@gmail.com>
2021-05-26 23:06:04 -0400

    Update parseInt-reloaded.js

--- parseInt-reloaded.js
+++ parseInt-reloaded.js
@@ -1,10 +1,10 @@
 function parseInt(string) {
-  const map = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "sever": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90 };
+  const map = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90 };
   const multiMap = { "hundred": 100, "thousand": 1000, "million": 1000000 };
   let result = [], total = 0;
 
   function translateToNumber(string) {
-    const arr = string.split('-');
+    let arr = string.split('-');
     let num = 0;
     if (arr.length > 1) {
       for (let i = 0; i < arr.length; i++) {
