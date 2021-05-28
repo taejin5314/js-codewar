e6195ed498f4163de8a4d3c33216a2dc94ca905b
taejin <taejin5314@gmail.com>
2021-05-27 21:52:56 -0400

    Update sum-by-factors.js

--- sum-by-factors.js
+++ sum-by-factors.js
@@ -3,6 +3,14 @@ function sumOfDivided(lst) {
   return [[2, 12]];
 }
 
+function primeNumber(max) {
+  for (let i = 2; i <= Math.sqrt(max); i++) {
+    if (max % i === 0) {
+
+    }
+  }
+}
+
 
 console.log(sumOfDivided([12, 15]), [[2, 12], [3, 27], [5, 15]]);
 console.log(sumOfDivided([15, 21, 24, 30, 45]), [[2, 54], [3, 135], [5, 90], [7, 21]]);
