fbf2b5bd309660f0348c8b9ddbf5ebad3f4de7d3
taejin <taejin5314@gmail.com>
2021-05-27 23:57:43 -0400

    Create all-balanced-parentheses.js

---
+++ all-balanced-parentheses.js
@@ -0,0 +1,9 @@
+function balancedParens(n) {
+  return [];
+}
+
+console.log(balancedParens(0).sort(), [""]);
+console.log(balancedParens(1).sort(), ["()"]);
+console.log(balancedParens(2).sort(), ["(())", "()()"]);
+console.log(balancedParens(3).sort(), ["((()))", "(()())", "(())()", "()(())", "()()()"]);
+console.log(balancedParens(4).sort(), ["(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"]);
