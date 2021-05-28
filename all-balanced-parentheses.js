3e3fbbd115f7c8e807ab098cb8d0b826f5dd118a
taejin <taejin5314@gmail.com>
2021-05-28 00:10:02 -0400

    Update all-balanced-parentheses.js

--- all-balanced-parentheses.js
+++ all-balanced-parentheses.js
@@ -1,5 +1,16 @@
 function balancedParens(n) {
-  return [];
+  let result = []
+  if (n === 0) result.push("");
+  else if (n === 1) result.push("()");
+  else if (n > 1) {
+    for (let elm of balancedParens(n - 1)) {
+      result.push("(" + elm + ")");
+      result.push("()" + elm);
+      result.push(elm + "()");
+    }
+  }
+
+  return result.filter((item, index) => result.indexOf(item) === index);
 }
 
 console.log(balancedParens(0).sort(), [""]);
