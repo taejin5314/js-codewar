ef337c7144ed307fea0b779e77ae36301a0c102f
taejin <taejin5314@gmail.com>
2021-05-28 15:11:01 -0400

    Update all-balanced-parentheses.js

--- all-balanced-parentheses.js
+++ all-balanced-parentheses.js
@@ -1,34 +1,23 @@
 function balancedParens(n) {
-  let str = new Array(2 * n);
-  if (n === 0) return [""]
-  return printParenthesis(str, n);
+  if (n === 0) return [''];
+  else if (n === 1) return ['()'];
+
+  let braces = balancedParens(n - 1);
+  let resultSet = new Set();
+  for (const s of braces) {
+    for (let j = 0; j < s.length; j++) {
+      let part1 = s.substring(0, j);
+      let part2 = s.substring(j);
+
+      resultSet.add(`${part1}()${part2}`);
+      resultSet.add(`(${part1})${part2}`);
+      resultSet.add(`${part1}(${part2})`);
+    }
+  }
+
+  return Array.from(resultSet.values()).sort();
 }
 
-function _printParenthesis(str, pos, n, open, close) {
-  if (close == n) {
-    // prvar the possible combinations
-    for (let i = 0; i < str.length; i++)
-      document.write(str[i]);
-    document.write("<br/>");
-    return;
-  }
-  else {
-    if (open > close) {
-      str[pos] = '}';
-      _printParenthesis(str, pos + 1, n, open, close + 1);
-    }
-    if (open < n) {
-      str[pos] = '{';
-      _printParenthesis(str, pos + 1, n, open + 1, close);
-    }
-  }
-}
-// Wrapper over _printParenthesis()
-function printParenthesis(str, n) {
-  if (n > 0)
-    _printParenthesis(str, 0, n, 0, 0);
-  return;
-}
 
 console.log(balancedParens(0).sort(), [""]);
 console.log(balancedParens(1).sort(), ["()"]);
