b6930a1b6cd54c60fddc1cad24ff5da4cadc7286
taejin <taejin5314@gmail.com>
2021-05-28 01:43:46 -0400

    Create sum-by-factors.js

---
+++ sum-by-factors.js
@@ -0,0 +1,8 @@
+function sumOfDivided(lst) {
+  //your code
+  return [[2, 12]];
+}
+
+
+console.log(sumOfDivided([12, 15]), [[2, 12], [3, 27], [5, 15]]);
+console.log(sumOfDivided([15, 21, 24, 30, 45]), [[2, 54], [3, 135], [5, 90], [7, 21]]);
