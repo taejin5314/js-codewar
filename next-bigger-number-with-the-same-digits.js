67227d35a53b6caf1c0bba45df1fa80d2b0d8c00
taejin <taejin5314@gmail.com>
2021-05-28 15:23:27 -0400

    Update next-bigger-number-with-the-same-digits.js

--- next-bigger-number-with-the-same-digits.js
+++ next-bigger-number-with-the-same-digits.js
@@ -1,5 +1,17 @@
 function nextBigger(n) {
-  //your code here
+  let arr = [];
+  for (let i = 0; i < factorial(n); i++) {
+    let combination = ''
+    n.toString().split('').forEach((elm) => {
+      console.log(elm, index);
+    })
+  }
+}
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) result *= i;
+  return n;
 }
 
 console.log(nextBigger(12), 21)
