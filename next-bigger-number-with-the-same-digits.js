1aba3e4c1122d500abdd8cc1fe15ea44f3e7d2ae
taejin <taejin5314@gmail.com>
2021-05-28 19:11:18 -0400

    Update next-bigger-number-with-the-same-digits.js

--- next-bigger-number-with-the-same-digits.js
+++ next-bigger-number-with-the-same-digits.js
@@ -1,17 +1,39 @@
 function nextBigger(n) {
-  let arr = [];
-  for (let i = 0; i < factorial(n); i++) {
-    let combination = ''
-    n.toString().split('').forEach((elm) => {
-      console.log(elm, index);
-    })
+  let d = n.toString().split('');
+  let p = -1;
+  for (let i = d.length - 1; i > 0; i--) {
+    if (+d[i] > +d[i - 1]) {
+      p = i - 1;
+      break;
+    }
   }
-}
 
-function factorial(n) {
-  let result = 1;
-  for (let i = 1; i <= n; i++) result *= i;
-  return n;
+  if (p === -1) return p;
+
+  let right = d.splice(p);
+  let pv = right.splice(0, 1)[0];
+  let mm = null, mmi = null;
+
+  for (let i = 0; i < right.length; i++) {
+    if (right[i] > pv) {
+      if (mm === null || right[i] < mm) {
+        mm = right[i];
+        mmi = i;
+      }
+    }
+  }
+
+  if (mmi === null) return -1;
+
+  right.splice(mmi, 1);
+  right.push(pv);
+  right = right.sort();
+
+  // concat the left + new pivot + right part
+  let ret = +d.concat([mm]).concat(right).join('');
+  if (ret < n) return -1;
+
+  return ret;
 }
 
 console.log(nextBigger(12), 21)
