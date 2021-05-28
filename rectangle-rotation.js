1e8956d05ab0e712f8b2e14fd096a1acb8467924
taejin <taejin5314@gmail.com>
2021-05-27 23:26:48 -0400

    Update rectangle-rotation.js

---
+++ rectangle-rotation.js
@@ -0,0 +1,8 @@
+function rectangleRotation(a, b) {
+  //coding and coding.. 
+}
+
+console.log(rectangleRotation(6, 4), 23)
+console.log(rectangleRotation(30, 2), 65)
+console.log(rectangleRotation(8, 6), 49)
+console.log(rectangleRotation(16, 20), 333)
