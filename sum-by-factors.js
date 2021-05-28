798f56d0cd677f3dddc2bbc87aad47da8d34c98e
taejin <taejin5314@gmail.com>
2021-05-27 23:03:45 -0400

    Update sum-by-factors.js

--- sum-by-factors.js
+++ sum-by-factors.js
@@ -1,6 +1,14 @@
 function sumOfDivided(lst) {
   //your code
-  return primeNumber(25);
+  let max = lst[0];
+  for (let elm of lst) {
+    if (Math.abs(elm) > Math.abs(max)) max = elm;
+  }
+  const primeArr = primeNumber(Math.abs(max));
+
+  for (let i = 0; i < lst.length; i++) {
+
+  }
 }
 
 function primeNumber(max) {
@@ -20,3 +28,4 @@ function primeNumber(max) {
 
 console.log(sumOfDivided([12, 15]), [[2, 12], [3, 27], [5, 15]]);
 console.log(sumOfDivided([15, 21, 24, 30, 45]), [[2, 54], [3, 135], [5, 90], [7, 21]]);
+console.log(sumOfDivided([-45, 5, 15]));
