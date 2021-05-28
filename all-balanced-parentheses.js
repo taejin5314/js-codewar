a016b1de3a6d216b87bb6861629d1d11ed4cf7cd
taejin <taejin5314@gmail.com>
2021-05-28 14:50:51 -0400

    Update all-balanced-parentheses.js

--- all-balanced-parentheses.js
+++ all-balanced-parentheses.js
@@ -1,16 +1,33 @@
 function balancedParens(n) {
-  let combinations = [];
-  let currentArr = [];
-  generateAllBalacnedParens(2 * n, currentArr, 0, combinations);
+  let str = new Array(2 * n);
+  if (n === 0) return [""]
+  return printParenthesis(str, n);
+}
 
-  function generateAllBalacnedParens(currentLength, currentArr, pos, result) {
-    if (pos === currentLength) {
-      if (valid(current)) result.push(currentArr);
-    } else {
-      currentArr[pos] = '(';
-      generateAllBalacnedParens(currentLength +)
+function _printParenthesis(str, pos, n, open, close) {
+  if (close == n) {
+    // prvar the possible combinations
+    for (let i = 0; i < str.length; i++)
+      document.write(str[i]);
+    document.write("<br/>");
+    return;
+  }
+  else {
+    if (open > close) {
+      str[pos] = '}';
+      _printParenthesis(str, pos + 1, n, open, close + 1);
+    }
+    if (open < n) {
+      str[pos] = '{';
+      _printParenthesis(str, pos + 1, n, open + 1, close);
     }
   }
+}
+// Wrapper over _printParenthesis()
+function printParenthesis(str, n) {
+  if (n > 0)
+    _printParenthesis(str, 0, n, 0, 0);
+  return;
 }
 
 console.log(balancedParens(0).sort(), [""]);
