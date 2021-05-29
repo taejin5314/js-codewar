32a01dc0a09384bc6bfef18906f88581e9ce39b0
taejin <taejin5314@gmail.com>
2021-05-29 14:54:05 -0400

    Update make-a-spiral.js

--- make-a-spiral.js
+++ make-a-spiral.js
@@ -1,20 +1,21 @@
 var spiralize = function (size) {
   // insert code here
-  snail(size)
-}
-
-snail = function (array) {
-  let arr = [], a = 0, b = -1;
-  const arrayLength = array.length;
-  if (array[0][0] === undefined) return arr;
-  for (let i = arrayLength * 2 - 1; i >= 1; i--) {
-    let direction = (arrayLength * 2 - 1 - i) % 4
-    for (let j = 0; j < Math.ceil(i / 2); j++) {
-      if (direction === 0 && b < arrayLength - 1) {
-        // going right
+  let a = 0, b = -1, arr = Array.from(Array(size), () => new Array(size).fill(0));
+  let even = (size % 2 === 0) ? false : true;
+  // console.log("array: ", arr);
+  for (let i = size; i >= 1; i--) {
+    let direction = (size - i) % 4;
+    if (i !== size && even) {
+      if (i % 2 !== 0) c = i + 1;
+      else c = i;
+    } else if (i !== size && !even) {
+      if (i % 2 === 0) c = i + 1;
+      else c = i;
+    } else c = i;
+    for (let j = 0; j < c; j++) {
+      if (direction === 0 && b < size - 1) {
         b++;
-      } else if (direction === 1 && a < arrayLength - 1) {
-        // going down
+      } else if (direction === 1 && a < size - 1) {
         a++;
       } else if (direction === 2 && b > 0) {
         // going left
@@ -23,7 +24,7 @@ snail = function (array) {
         // going up
         a--;
       }
-      arr.push(array[a][b]);
+      arr[a][b] = 1;
     }
   }
   return arr;
