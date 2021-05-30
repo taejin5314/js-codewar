36f99793f0d359c7ff284fbdd81b323869d3156d
taejin <taejin5314@gmail.com>
2021-05-30 16:55:52 -0400

    Update last-digit-of-a-huge-number.js

--- last-digit-of-a-huge-number.js
+++ last-digit-of-a-huge-number.js
@@ -1,10 +1,25 @@
+function trueMod(a, n, m) {
+  return Math.round((a % m) * Math.pow((a % m), (n + 3) % 4)) % m;
+}
+
 function lastDigit(as) {
-  let n = 1;
-  if (as.length === 0) return 1;
-  for (let i = as.length - 1; i >= 0; i--) {
-    n = Math.pow(as[i], n) % 4;
+  if (as.length == 0) return 1;
+  let rightIsZero = false;
+  let rightBiggerThan2 = false;
+  let rightMod4 = 1;
+  for (let i = as.length - 1; i > 0; --i) {
+    if (rightIsZero) {
+      rightMod4 = 1;
+      rightIsZero = false;
+      rightBiggerThan2 = false;
+    } else {
+      rightMod4 = (rightBiggerThan2 && (as[i] % 4 === 2)) ? 0 : trueMod(as[i], rightMod4, 4);
+      rightIsZero = as[i] === 0;
+      rightBiggerThan2 = !rightIsZero && !(as[i] === 1)
+    }
   }
-  return n;
+
+  return rightIsZero ? 1 : trueMod(as[0], rightMod4, 10);
 }
 
 console.log(lastDigit([]), 1);
