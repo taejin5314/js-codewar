ed50202888c7381acbc6a4854aee34de988da770
taejin <taejin5314@gmail.com>
2021-05-30 17:04:49 -0400

    Create how-many-are-smaller-than-me-II.js

---
+++ how-many-are-smaller-than-me-II.js
@@ -0,0 +1,20 @@
+function smaller(arr) {
+  //code me
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]);
+      count++;
+    }
+    result.push(count);
+  }
+  return result;
+}
+
+console.log(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
+console.log(smaller([1, 2, 3]), [0, 0, 0]);
+console.log(smaller([1, 2, 0]), [1, 1, 0]);
+console.log(smaller([1, 2, 1]), [0, 1, 0]);
+console.log(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0]);
+console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0]);
