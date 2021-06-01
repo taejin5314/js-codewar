dc1c254a071f7e1fa0e99b973a8676e6fc2ed658
taejin <taejin5314@gmail.com>
2021-06-01 18:04:31 -0400

    Update binomial-expansion.js

--- binomial-expansion.js
+++ binomial-expansion.js
@@ -5,8 +5,17 @@ function expand(expr) {
   if (n === 0) return '1';
   else if (n === 1) return arr[0].slice(1, -1);
   else {
-    if (arr[0][1] === '-') a = parseInt(arr[0].slice(1, 3))
-    console.log(a);
+    if (arr[0][1] === '-') {
+      console.log(parseInt(arr[0][2]))
+      if (parseInt(arr[0][2]) === NaN) {
+        a = -1;
+        x = arr[0][2];
+        b = parseInt(arr[0].slice(3))
+      } else {
+        a = parseInt(arr[0].slice(1, 3))
+      }
+    }
+    console.log(a, x, b);
   }
 }
 
@@ -15,7 +24,7 @@ console.log(expand("(x+1)^1"), "x+1");
 console.log(expand("(x+1)^2"), "x^2+2x+1");
 console.log(expand("(x-1)^0"), "1");
 console.log(expand("(x-1)^1"), "x-1");
-console.log(expand("(x-1)^2"), "x^2-2x+1");
+console.log(expand("(-x-1)^2"), "x^2-2x+1");
 console.log(expand("(5m+3)^4"), "625m^4+1500m^3+1350m^2+540m+81");
 console.log(expand("(2x-3)^3"), "8x^3-36x^2+54x-27");
 console.log(expand("(7x-7)^0"), "1");
