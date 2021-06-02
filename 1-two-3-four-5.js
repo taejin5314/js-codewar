02d358a1a7b8b2f45ad350a54d122227190e1fda
taejin <taejin5314@gmail.com>
2021-06-02 15:36:23 -0400

    Create 1-two-3-four-5.js

---
+++ 1-two-3-four-5.js
@@ -0,0 +1,12 @@
+function conv(num) {
+  //Good luck :)
+}
+
+console.log(conv(0), "0")
+console.log(conv(11), "11")
+console.log(conv(1101), "11zer1")
+console.log(conv(54563), "F4FIV6THREE")
+console.log(conv(47309534), "f73zero953fourFOUR")
+console.log(conv(34266262106), "T4266262ONEoneONE06")
+console.log(conv(15795379351687), "15795379351sixSIXsixSIXeightEIGHTeig7")
+console.log(conv(157953793516872), "OFISEVNINEFIVEfTHREEtSEVENseNINEnineTHREEthreFIVEfiveFIONEoneONEon68SEVENsevenSEVE2")
