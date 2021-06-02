510bdb8ba9c61daf7fa73cf8e38147edbd2bdcb0
taejin <taejin5314@gmail.com>
2021-06-02 10:44:50 -0400

    Update binomial-expansion.js

--- binomial-expansion.js
+++ binomial-expansion.js
@@ -1,50 +1,46 @@
 function expand(expr) {
   const arr = expr.split('^');
   const n = parseInt(arr[1]);
+  let elmArray = [];
   let a, x, b;
   let result = '';
   if (n === 0) return '1';
   else if (n === 1) return arr[0].slice(1, -1);
   else {
-    if (arr[0][1] === '-') {
-      if (isNaN(parseInt(arr[0][2]))) {
-        a = -1;
-        x = arr[0][2];
-        b = parseInt(arr[0].slice(3))
-      } else {
-        a = parseInt(arr[0].slice(1, 3));
-        x = arr[0][3];
-        b = parseInt(arr[0].slice(4))
-      }
-    } else {
-      if (isNaN(parseInt(arr[0][1]))) {
-        a = 1;
-        x = arr[0][1];
-        b = parseInt(arr[0].slice(2));
-      } else {
-        a = parseInt(arr[0].slice(1, 2));
-        x = arr[0][2];
-        b = parseInt(arr[0].slice(3));
+    for (let elm of arr[0].split(/[a-z]|\(|\)|\+/)) {
+      if (elm !== '') {
+        if (elm === '-') elm = -1;
+        elmArray.push(parseInt(elm));
       }
     }
-
-    for (let i = n; i >= 0; i--) {
-      if (i !== 0) {
-        const temp = Math.pow(a, i) * Math.pow(b, n - i) * combinations(n, i)
-        if (i !== n && temp > 0) result += '+' + temp;
-        else if (i === n && temp !== 1) result += temp;
-        else if (temp < 0) result += temp;
-        else result += '';
-        result += (i !== 1) ? x + '^' + i : x;
-      } else {
-        if (Math.pow(b, n - i) > 0) result += '+' + Math.pow(b, n - i);
-        else if (b === 0) result += '';
-        else result += Math.pow(b, n - i);
-      }
-    }
-    return result;
+    if (elmArray.length < 2) elmArray.unshift(1);
+    elmArray.push(arr[0].split(/\(|\d|\)|\-|\+/).join('').trim());
+    a = elmArray[0];
+    b = elmArray[1];
+    x = elmArray[2];
   }
+
+  for (let i = n; i >= 0; i--) {
+    if (i !== 0) {
+      const temp = Math.pow(a, i) * Math.pow(b, n - i) * combinations(n, i)
+      console.log(temp);
+      if (i !== n && temp > 0) result += '+' + temp;
+      else if (i === n && temp !== 1) result += temp;
+      else if (temp < 0) result += temp;
+      else {
+        if (temp === -1) result += '-';
+        result += '';
+      }
+      result += (i !== 1) ? x + '^' + i : x;
+    } else {
+      if (Math.pow(b, n - i) > 0) result += '+' + Math.pow(b, n - i);
+      else if (b === 0) result += '';
+      else result += Math.pow(b, n - i);
+    }
+  }
+  return result;
 }
+
 
 
 function factorial(a) {
@@ -56,15 +52,15 @@ function combinations(n, r) {
   return factorial(n) / (factorial(r) * factorial(n - r))
 }
 
-console.log(expand("(x+1)^0"), "1");
+console.log(expand("(x+0)^0"), "1");
 console.log(expand("(x+1)^1"), "x+1");
-console.log(expand("(x+1)^2"), "x^2+2x+1");
+console.log(expand("(x+0)^2"), "x^2");
 console.log(expand("(x-1)^0"), "1");
 console.log(expand("(x-1)^1"), "x-1");
-console.log(expand("(-m-1)^2"), "x^2-2x+1");
+console.log(expand("(-m-1)^3"), "x^2-2x+1");
 console.log(expand("(5m+3)^4"), "625m^4+1500m^3+1350m^2+540m+81");
 console.log(expand("(2x-3)^3"), "8x^3-36x^2+54x-27");
 console.log(expand("(7x-7)^0"), "1");
 console.log(expand("(-5m+3)^4"), "625m^4-1500m^3+1350m^2-540m+81");
 console.log(expand("(-2k-3)^3"), "-8k^3-36k^2-54k-27");
-console.log(expand("(-7x-7)^0"), "1");
+console.log(expand("(-246247x-77247)^3"), "1");
