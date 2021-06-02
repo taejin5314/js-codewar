4922a4276fc99c5613ef5949e89ccb9e074d1a67
taejin <taejin5314@gmail.com>
2021-06-02 14:12:26 -0400

    Update ludicrous-coloured-triangles.js

--- ludicrous-coloured-triangles.js
+++ ludicrous-coloured-triangles.js
@@ -1,36 +1,24 @@
 const colors = ["R", "G", "B"];
 
-function combineColors(color1, color2) {
-  if (color1 === color2) {
-    return color1;
-  }
-
-  return colors.find(color => color !== color1 && color !== color2);
-}
-
-function log3(x) {
-  return Math.log(x) / Math.log(3);
-}
-
 function triangle(row) {
-  const { length } = row;
+  console.log(row);
+  const triangleLength = row.length;
+  const n = Math.floor(Math.log(triangleLength) / Math.log(3));
+  const max = Math.pow(3, n) + 1;
 
-  if (length === 1) {
-    return row[0];
-  }
+  if (triangleLength === 1) return row[0];
+  if (triangleLength === max) return colorCheck(row[0], row[triangleLength - 1]);
 
-  const n = Math.floor(log3(length - 1));
-  const idealLength = Math.pow(3, n) + 1;
-
-  if (length === idealLength) {
-    return combineColors(row[0], row[length - 1]);
-  }
-
-  const size = length - idealLength + 1;
+  const size = triangleLength - max + 1;
   const firstColor = triangle(row.substring(0, size));
-  const secondColor = triangle(row.substring(length - size));
+  const secondColor = triangle(row.substring(triangleLength - size));
 
-  return combineColors(firstColor, secondColor);
+  return colorCheck(firstColor, secondColor);
+}
+
+function colorCheck(color1, color2) {
+  if (color1 === color2) return color1;
+  return colors.find(color => color !== color1 && color !== color2);
 }
 
 console.log(triangle('B'), 'B');
