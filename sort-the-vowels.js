f00760919840fe2d7774caf6ef6ae2658bceb1b7
taejin <taejin5314@gmail.com>
2021-06-04 12:31:48 -0400

    Create sort-the-vowels.js

---
+++ sort-the-vowels.js
@@ -0,0 +1,8 @@
+function sortVowels(s) {
+  //code
+}
+
+console.log(sortVowels('Codewars'), 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|');
+console.log(sortVowels('Rnd Te5T'), 'R|\nn|\nd|\n |\nT|\n|e\n5|\nT|');
+console.log(sortVowels(1337), '');
+console.log(sortVowels(undefined), '');
