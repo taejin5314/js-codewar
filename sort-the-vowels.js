27218d3e62ec1712ec4b74267666982235176140
taejin <taejin5314@gmail.com>
2021-06-04 15:14:42 -0400

    Update sort-the-vowels.js

--- sort-the-vowels.js
+++ sort-the-vowels.js
@@ -1,5 +1,11 @@
 function sortVowels(s) {
   //code
+  if (typeof s === 'undefined' || typeof s === 'null' || typeof s === 'number') return '';
+  let arr = s.split('');
+  for (elm of arr) {
+    let match = elm.match(/[a-z]|[A-Z]/);
+    console.log(match[0]);
+  }
 }
 
 console.log(sortVowels('Codewars'), 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|');
