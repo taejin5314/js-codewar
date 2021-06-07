11051f810c6b41a9f2955cbf241750eb608c4466
taejin <taejin5314@gmail.com>
2021-06-06 22:17:16 -0400

    Update simple-encryption-1.js

--- simple-encryption-1.js
+++ simple-encryption-1.js
@@ -1,9 +1,25 @@
 function encrypt(text, n) {
-
+  for (let i = 0; i < n; i++) {
+    let newStr = '', remain = '';
+    text.split('').map((elm, index) => {
+      if (index % 2 === 1) newStr += elm;
+      else remain += elm;
+    })
+    text = newStr.concat(remain);
+  }
+  return text;
 }
 
 function decrypt(encryptedText, n) {
-
+  for (let i = 0; i < n; i++) {
+    let newStr = '';
+    for (let j = 0; j < Math.floor(encryptedText.length / 2); j++) {
+      newStr += encryptedText[Math.floor(encryptedText.length / 2) + j];
+      newStr += encryptedText[j];
+    }
+    encryptedText = newStr;
+  }
+  return encryptedText;
 }
 
 console.log(encrypt("This is a test!", 0), "This is a test!");
