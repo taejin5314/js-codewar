175a7c349c716534a3696507446175010b3b1f30
taejin <taejin5314@gmail.com>
2021-06-07 14:45:42 -0400

    Create build-tower.js

---
+++ build-tower.js
@@ -0,0 +1,7 @@
+function towerBuilder(nFloors) {
+  // build here
+}
+
+console.log(towerBuilder(1), ["*"]);
+console.log(towerBuilder(2), [" * ", "***"]);
+console.log(towerBuilder(3), ["  *  ", " *** ", "*****"]);
