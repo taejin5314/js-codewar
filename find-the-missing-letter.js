6d087a3732482fc087dd6ba84fd698dac67d50f9
taejin <taejin5314@gmail.com>
2021-06-07 14:20:18 -0400

    Create find-the-missing-letter.js

---
+++ find-the-missing-letter.js
@@ -0,0 +1,6 @@
+function findMissingLetter(array) {
+  return ' ';
+}
+
+Test.assertEquals(findMissingLetter(['a', 'b', 'c', 'd', 'f']), 'e');
+Test.assertEquals(findMissingLetter(['O', 'Q', 'R', 'S']), 'P');
