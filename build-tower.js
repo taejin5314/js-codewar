e4f28b04cc75514cb88cb7bff60ee65ecb69a926
taejin <taejin5314@gmail.com>
2021-06-07 15:08:47 -0400

    Update build-tower.js

--- build-tower.js
+++ build-tower.js
@@ -1,5 +1,15 @@
 function towerBuilder(nFloors) {
-  // build here
+  if (nFloors === 1) return ["*"];
+  let result = [];
+  for (let i = 0; i < nFloors; i++) {
+    let stars = '';
+    for (let j = 0; j < nFloors * 2 - 1; j++) {
+      if ((nFloors - i) - 1 <= j && j < (nFloors + i)) stars += '*';
+      else stars += ' ';
+    }
+    result.push(stars);
+  }
+  return result;
 }
 
 console.log(towerBuilder(1), ["*"]);
