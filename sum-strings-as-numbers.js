db0ac0ca90e1ca69582ce0d4f8e2d8c9bc298551
taejin <taejin5314@gmail.com>
2021-06-08 16:03:20 -0400

    Update sum-strings-as-numbers.js

--- sum-strings-as-numbers.js
+++ sum-strings-as-numbers.js
@@ -1,7 +1,13 @@
 function sumStrings(a, b) {
-  let A = a === '' ? 0 : BigInt(a);
-  let B = b === '' ? 0 : BigInt(b);
-  return (A + B).toString();
+  let result = [], x = 0;
+  for (let i = 1; i <= Math.max(a.length, b.length); i++) {
+    if (a[a.length - i] && b[b.length - i]) result.unshift((parseInt(a[a.length - i]) + parseInt(b[b.length - i]) + x) % 10);
+    else if (a[a.length - i]) result.unshift(parseInt(a[a.length - i]));
+    else if (b[b.length - i]) result.unshift(parseInt(b[b.length - i]));
+    if (parseInt(a[a.length - i]) + parseInt(b[b.length - i] + x) > 9) x = 1;
+    else x = 0;
+  }
+  return result.join('');
 }
 
 console.log(sumStrings('123', '456'), '579')
