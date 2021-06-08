ce461ff46067a9bed3a4372a5d844f4f5e2f8d52
taejin <taejin5314@gmail.com>
2021-06-08 19:19:53 -0400

    Update write-number-in-expanded-form.js

--- write-number-in-expanded-form.js
+++ write-number-in-expanded-form.js
@@ -1,5 +1,14 @@
 function expandedForm(num) {
-
+  let numLength = num.toString().length;
+  let result = '';
+  for (let i = numLength - 1; i >= 0; i--) {
+    if (Math.floor(num / Math.pow(10, i)) > 0) {
+      result += Math.floor(num / Math.pow(10, i)) * Math.pow(10, i) + ' + ';
+      num -= Math.floor(num / Math.pow(10, i)) * Math.pow(10, i)
+      numLength = num.toString().length;
+    }
+  }
+  return result.slice(0, -3);
 }
 
 console.log(expandedForm(12), '10 + 2');
