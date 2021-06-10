e5999b58f50cc325348cdf77688f4fe056643db5
taejin <taejin5314@gmail.com>
2021-06-10 16:33:59 -0400

    Create permutation.js

---
+++ permutation.js
@@ -0,0 +1,7 @@
+function permutations(string) {
+
+}
+
+console.log(permutations('a'), ['a']);
+console.log(permutations('ab').sort(), ['ab', 'ba'].sort());
+console.log(permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort());
