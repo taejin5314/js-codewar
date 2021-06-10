27e46678b2f99d1dd5107808c528c28046681845
taejin <taejin5314@gmail.com>
2021-06-10 17:00:18 -0400

    Update permutation.js

--- permutation.js
+++ permutation.js
@@ -1,5 +1,25 @@
 function permutations(string) {
+  let result = [];
 
+  const permute = (str, temp = '') => {
+    let array = str.split('');
+    if (array.length === 0) {
+      result.push(temp);
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        let cur = array.slice();
+        let next = cur.splice(i, 1);
+        permute(cur.slice().join(''), temp + next.join(''));
+      }
+    }
+  }
+
+  const unique = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+
+  permute(string);
+  return result.filter(unique)
 }
 
 console.log(permutations('a'), ['a']);
