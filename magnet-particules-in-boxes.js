1b74b3af02e1c4a8574ac402f0960b16a0f41b24
taejin <taejin5314@gmail.com>
2021-06-11 16:03:41 -0400

    Create magnet-particules-in-boxes.js

---
+++ magnet-particules-in-boxes.js
@@ -0,0 +1,8 @@
+function doubles(maxk, maxn) {
+  // your code
+}
+
+console.log(doubles(1, 10), 0.5580321939764581);
+console.log(doubles(10, 1000), 0.6921486500921933);
+console.log(doubles(10, 10000), 0.6930471674194457);
+console.log(doubles(20, 10000), 0.6930471955575918);
