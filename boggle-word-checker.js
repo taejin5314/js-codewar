6ca324d162a6f7bac552ad40a8cd00dcd2bda27e
taejin <taejin5314@gmail.com>
2021-06-11 18:30:24 -0400

    Update boggle-word-checker.js

--- boggle-word-checker.js
+++ boggle-word-checker.js
@@ -24,7 +24,7 @@ const checkAdjacent = (board, target, i, j) => {
     if ((i > 0 && j < board[i].length - 1) && board[i - 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j + 1);
     if (j > 0 && board[i][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j - 1);
     if (j < board[i].length - 1 && board[i][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j + 1);
-    if (i < board.length - 1 && board[i + 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j - 1);
+    if ((i < board.length - 1 && j > 0) && board[i + 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j - 1);
     if (i < board.length - 1 && board[i + 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j);
     if ((i < board.length - 1 && j < board[i].length - 1) && board[i + 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j + 1);
   }
