00991124baab3aa5bb1d98e36660c882b1958169
taejin <taejin5314@gmail.com>
2021-06-11 19:31:06 -0400

    Update boggle-word-checker.js

--- boggle-word-checker.js
+++ boggle-word-checker.js
@@ -1,11 +1,55 @@
 function checkWord(board, word) {
   const startPoint = word[0];
-  let result;
+  let result = false, direction = [];
+
+  // console.log([[1, 2], [1, 3], [2, 1]].findIndex(elm => JSON.stringify(elm) === JSON.stringify([2, 1])))
+
+  const checkAdjacent = (board, target, i, j) => {
+    if (target.length === 0) {
+      result = true;
+      return;
+    }
+    else {
+      if ((i > 0 && j > 0) && board[i - 1][j - 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i - 1, j - 1])) === -1) {
+        direction.push([i - 1, j - 1]);
+        checkAdjacent(board, target.slice(1), i - 1, j - 1);
+      }
+      if (i > 0 && board[i - 1][j] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i - 1, j])) === -1) {
+        direction.push([i - 1, j])
+        checkAdjacent(board, target.slice(1), i - 1, j);
+      }
+      if ((i > 0 && j < board[i].length - 1) && board[i - 1][j + 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i - 1, j + 1])) === -1) {
+        direction.push([i - 1, j + 1])
+        checkAdjacent(board, target.slice(1), i - 1, j + 1);
+      }
+      if (j > 0 && board[i][j - 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i, j - 1])) === -1) {
+        direction.push([i, j - 1])
+        checkAdjacent(board, target.slice(1), i, j - 1);
+      }
+      if (j < board[i].length - 1 && board[i][j + 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i, j + 1])) === -1) {
+        direction.push([i, j + 1])
+        checkAdjacent(board, target.slice(1), i, j + 1);
+      }
+      if ((i < board.length - 1 && j > 0) && board[i + 1][j - 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i + 1, j - 1])) === -1) {
+        direction.push([i + 1, j - 1])
+        checkAdjacent(board, target.slice(1), i + 1, j - 1);
+      }
+      if (i < board.length - 1 && board[i + 1][j] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i + 1, j])) === -1) {
+        direction.push([i + 1, j])
+        checkAdjacent(board, target.slice(1), i + 1, j);
+      }
+      if ((i < board.length - 1 && j < board[i].length - 1) && board[i + 1][j + 1] === target[0] && direction.findIndex(elm => JSON.stringify(elm) === JSON.stringify([i + 1, j + 1])) === -1) {
+        direction.push([i + 1, j + 1]);
+        checkAdjacent(board, target.slice(1), i + 1, j + 1);
+      }
+    }
+  }
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (startPoint === board[i][j]) {
-        result = checkAdjacent(board, word.slice(1), i, j);
+        direction.push([i, j]);
+        checkAdjacent(board, word.slice(1), i, j);
       }
     }
   }
@@ -13,22 +57,6 @@ function checkWord(board, word) {
   return result;
 }
 
-const checkAdjacent = (board, target, i, j) => {
-  let count = 0;
-  if (target.length === 0) {
-    return true;
-  }
-  else {
-    if ((i > 0 && j > 0) && board[i - 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j - 1);
-    if (i > 0 && board[i - 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j);
-    if ((i > 0 && j < board[i].length - 1) && board[i - 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j + 1);
-    if (j > 0 && board[i][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j - 1);
-    if (j < board[i].length - 1 && board[i][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j + 1);
-    if ((i < board.length - 1 && j > 0) && board[i + 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j - 1);
-    if (i < board.length - 1 && board[i + 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j);
-    if ((i < board.length - 1 && j < board[i].length - 1) && board[i + 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j + 1);
-  }
-}
 
 var testBoard = [
   ["E", "A", "R", "A"],
