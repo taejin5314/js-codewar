edecd9efa751da2d67382aceccb64e0be7cde003
taejin <taejin5314@gmail.com>
2021-06-11 17:54:00 -0400

    Update boggle-word-checker.js

--- boggle-word-checker.js
+++ boggle-word-checker.js
@@ -17,13 +17,13 @@ const checkAdjacent = (board, target, i, j) => {
   if (target.length === 0) return true;
   else {
     if ((i > 0 && j > 0) && board[i - 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j - 1);
-    else if (i > 0 && board[i - 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j);
-    else if ((i > 0 && j < board[i].length - 1) && board[i - 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j + 1);
-    else if (j > 0 && board[i][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j - 1);
-    else if (j < board[i].length - 1 && board[i][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j + 1);
-    else if (i < board.length - 1 && board[i + 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j - 1);
-    else if (i < board.length - 1 && board[i + 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j);
-    else if ((i < board.length - 1 && j < board[i].length - 1) && board[i + 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j + 1);
+    if (i > 0 && board[i - 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j);
+    if ((i > 0 && j < board[i].length - 1) && board[i - 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i - 1, j + 1);
+    if (j > 0 && board[i][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j - 1);
+    if (j < board[i].length - 1 && board[i][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i, j + 1);
+    if (i < board.length - 1 && board[i + 1][j - 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j - 1);
+    if (i < board.length - 1 && board[i + 1][j] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j);
+    if ((i < board.length - 1 && j < board[i].length - 1) && board[i + 1][j + 1] === target[0]) return checkAdjacent(board, target.slice(1), i + 1, j + 1);
   }
   return false;
 }
