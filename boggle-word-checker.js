69f0252270fefbe5f0a960dee46c9ec3afb067c7
taejin <taejin5314@gmail.com>
2021-06-11 16:28:42 -0400

    Update boggle-word-checker.js

---
+++ boggle-word-checker.js
@@ -0,0 +1,11 @@
+
+
+console.log(checkWord(testBoard, "C") == true);
+console.log(checkWord(testBoard, "EAR") == true);
+console.log(checkWord(testBoard, "EARS") == false);
+console.log(checkWord(testBoard, "BAILER") == true);
+console.log(checkWord(testBoard, "RSCAREIOYBAILNEA") == true, "Must be able to check indefinite word lengths going in all directions");
+console.log(checkWord(testBoard, "CEREAL") == false, "Valid guesses can't overlap themselves");
+console.log(checkWord(testBoard, "ROBES") == false, "Valid guesses have to be adjacent");
+console.log(checkWord(testBoard, "BAKER") == false, "All the letters have to be in the board");
+console.log(checkWord(testBoard, "CARS") == false, "Valid guesses cannot wrap around the edges of the board");
