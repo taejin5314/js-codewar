3fbf5c3c6bb5acc79ca7bfabe0cd20d9b284cfa8
taejin <taejin5314@gmail.com>
2021-06-13 17:27:12 -0400

    Update reverse-an-array.js

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -1,7 +1,4 @@
-const reverse = a => {
-  let b = a.map(elm => console.log(elm), arr = []);
-  console.log(b);
-}
+const reverse = a => a.map(a.pop, [...a])
 
 console.log(reverse([1, 2, 3]), [3, 2, 1]);
 console.log(reverse([...'01234567890123456789']), [...'98765432109876543210']);
