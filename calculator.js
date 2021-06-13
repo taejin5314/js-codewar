42e25dc1abefec0a684bb1d8a33cf12574059c47
taejin <taejin5314@gmail.com>
2021-06-12 20:36:45 -0400

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const Calculator = function () {
+  this.evaluate = string => {
+
+  }
+};
+
+console.log(calculate.evaluate('127'), 127);
+console.log(calculate.evaluate('2 + 3'), 5);
+console.log(calculate.evaluate('2 - 3 - 4'), -5);
+console.log(calculate.evaluate('10 * 5 / 2'), 25);
