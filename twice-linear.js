25686847fc8540f34ddcba810e8f58b966a6cf2d
taejin <taejin5314@gmail.com>
2021-06-14 16:03:49 -0400

    Create twice-linear.js

---
+++ twice-linear.js
@@ -0,0 +1,9 @@
+function dblLinear(n) {
+
+}
+
+console.log(dblLinear(10), 22)
+console.log(dblLinear(20), 57)
+console.log(dblLinear(30), 91)
+console.log(dblLinear(50), 175)
+console.log(dblLinear(100), 447)
