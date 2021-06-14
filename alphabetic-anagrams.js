13b323f4135c33c928667ad851c584d4a937e2c4
taejin <taejin5314@gmail.com>
2021-06-14 15:21:25 -0400

    Update alphabetic-anagrams.js

--- alphabetic-anagrams.js
+++ alphabetic-anagrams.js
@@ -1,7 +1,36 @@
 function listPosition(word) {
-  //Return the anagram list position of the word
-  return 1;
+  const sortedLetters = word.split('').sort();
+  return word.split('').reduce((acc, letter) => {
+    const index = sortedLetters.indexOf(letter)
+
+    sortedLetters.splice(index, 1)
+
+    const lettersAhead = [...new Set(sortedLetters.slice(0, index))]
+
+    return (
+      acc +
+      lettersAhead.reduce(
+        (a, l) =>
+          a + calculatePermutations(sortedLetters.join('').replace(l, letter)),
+        0
+      )
+    )
+  }, 1)
 }
+
+function calculatePermutations()
+
+function factorial(n) {
+  let f = [1, 1]
+  if (f[n] !== undefined) return f[n];
+
+  for (i = 2; i <= n; i++) {
+    f[i] = f[i - 1] * i;
+  }
+
+  return f[n]
+}
+
 
 var testValues = { 'A': 1, 'ABAB': 2, 'AAAB': 1, 'BAAA': 4, 'QUESTION': 24572, 'BOOKKEEPER': 10743 };
 for (var word in testValues) {
