842a699171980f8514105b2e07dd23c1163eab95
taejin <taejin5314@gmail.com>
2021-06-13 21:31:55 -0400

    Update the-millionth-fibonacci-kata.js

--- the-millionth-fibonacci-kata.js
+++ the-millionth-fibonacci-kata.js
@@ -1,12 +1,15 @@
 function fib(n) {
   if (n === 0 || n === 1) return BigInt(n);
-  else if (n >= 2) {
-    a = 0n;
-    b = 1n;
-    for (let i = 0; i < n; i++) [a, b] = [a + b, a]
-    return a;
-  }
-  else {
+  else if (n >= 2 && n % 2 == 0) {
+    let k = n / 2;
+    let fk = fib(k);
+    return (2n * fib(k - 1) + fk) * fk;
+  } else if (n >= 2) {
+    let k = (n + 1) / 2;
+    let fk1 = fib(k - 1);
+    let fk2 = fib(k);
+    return fk2 * fk2 + fk1 * fk1;
+  } else {
     a = 0n;
     b = 1n;
     for (let i = 0; i > n; i--) [a, b] = [b - a, a]
@@ -21,3 +24,4 @@ console.log(fib(3), 2n)
 console.log(fib(4), 3n)
 console.log(fib(5), 5n)
 console.log(fib(-6), -8n)
+console.log(fib(1914352))
