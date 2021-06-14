9ac72ebef151c2fdf747d168ccbc1ba02a7a5185
taejin <taejin5314@gmail.com>
2021-06-13 21:11:50 -0400

    Create the-millionth-fibonacci-kata.js

---
+++ the-millionth-fibonacci-kata.js
@@ -0,0 +1,11 @@
+function fib(n) {
+  return 0n;
+}
+
+console.log(fib(0), 0n)
+console.log(fib(1), 1n)
+console.log(fib(2), 1n)
+console.log(fib(3), 2n)
+console.log(fib(4), 3n)
+console.log(fib(5), 5n)
+console.log(fib(-6), -8n)
