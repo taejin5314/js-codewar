dbf1605a8e5c406edfd3aad4a0e9df0fd4730a34
taejin <taejin5314@gmail.com>
2021-06-14 16:49:59 -0400

    Update twice-linear.js

--- twice-linear.js
+++ twice-linear.js
@@ -1,10 +1,12 @@
 function dblLinear(n) {
   const dbl = [1];
+  let i = 0;
 
   if (dbl[n] !== undefined) return dbl[n];
   while (dbl[n] === undefined) {
-    dbl.push(dbl[n] * 2 + 1);
-    dbl.push(dbl[n] * 3 + 1);
+    dbl.push(dbl[i] * 2 + 1);
+    dbl.push(dbl[i] * 3 + 1);
+    i++;
   }
   return dbl.sort()[n];
 
