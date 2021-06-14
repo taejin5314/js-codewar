59d42c5831bcc23fb9bdca418ac62d0df70949cb
taejin <taejin5314@gmail.com>
2021-06-14 17:22:22 -0400

    Update twice-linear.js

--- twice-linear.js
+++ twice-linear.js
@@ -1,16 +1,23 @@
 function dblLinear(n) {
-  let dbl = [1];
-  let i = 0;
+  let dbl = [1], x = 0, y = 0;
 
-  if (dbl[n] !== undefined) return dbl[n];
-  while (dbl[n] === undefined) {
-    dbl.indexOf(dbl[i] * 2 + 1) === -1 ? dbl.push(dbl[i] * 2 + 1) : dbl;
-    dbl.indexOf(dbl[i] * 3 + 1) === -1 ? dbl.push(dbl[i] * 3 + 1) : dbl;
-    i++;
+  while (dbl.length <= n) {
+    let a = 2 * dbl[x] + 1;
+    let b = 3 * dbl[y] + 1;
+
+    if (a > b) {
+      dbl.push(b);
+      y++;
+    } else if (a < b) {
+      dbl.push(a);
+      x++;
+    } else {
+      dbl.push(a);
+      x++;
+      y++;
+    }
   }
-  dbl.sort(function (a, b) { return a - b });
   return dbl[n];
-
 }
 
 console.log(dblLinear(10), 22)
