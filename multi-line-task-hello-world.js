2a05d1fe0feae25c668b9c9f84e37e02af8aafc6
taejin <taejin5314@gmail.com>
2021-06-16 17:25:21 -0400

    Update multi-line-task-hello-world.js

--- multi-line-task-hello-world.js
+++ multi-line-task-hello-world.js
@@ -1,15 +1,43 @@
-f = _ => '\
+f =
+  [
+    '\
+m\
+a\
+p'
+  ][
+    '\
+c\
+o\
+n\
+s\
+t\
+r\
+u\
+c\
+t\
+o\
+r'
+  ]`
+r\
+e\
+t\
+u\
+r\
+n\
+'\
 H\
 e\
 l\
 l\
 o\
+,\
  \
 w\
 o\
 r\
 l\
 d\
-!'
+!\
+'`
 
 console.log(f(), 'Hello, world!');
