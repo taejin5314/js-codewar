5725a9effc1cb8f6e99ec494b471446b1b037a06
taejin <taejin5314@gmail.com>
2021-06-16 22:15:44 -0400

    Update multi-line-task-infinite-hello-world.js

--- multi-line-task-infinite-hello-world.js
+++ multi-line-task-infinite-hello-world.js
@@ -1,4 +1,116 @@
-[, H, , e, , l, , o, , w, , r, , d, , z, , y, , x, , t, , i, , m, , b, , n,] = ` H e l o w r d ,   ! t i m b n `
-f = ``[t + r + i + m][b + i + n + d](H + e + l + l + o + z + y + w + o + r + l + d + x)
+[
+  ,
+  H
+  ,
+  ,
+  e
+  ,
+  ,
+  l
+  ,
+  ,
+  o
+  ,
+  ,
+  w
+  ,
+  ,
+  r
+  ,
+  ,
+  d
+  ,
+  ,
+  z
+  ,
+  ,
+  y
+  ,
+  ,
+  x
+  ,
+  ,
+  t
+  ,
+  ,
+  i
+  ,
+  ,
+  m
+  ,
+  ,
+  b
+  ,
+  ,
+  n
+  ,
+]
+  =
+  `
+H
+e
+l
+o
+w
+r
+d
+,
+ 
+!
+t
+i
+m
+b
+n
+`
+f
+  =
+  `
+`
+  [
+    t
+    +
+    r
+    +
+    i
+    +
+    m
+  ]
+  [
+    b
+    +
+    i
+    +
+    n
+    +
+    d
+  ]
+    (
+      H
+      +
+      e
+      +
+      l
+      +
+      l
+      +
+      o
+      +
+      z
+      +
+      y
+      +
+      w
+      +
+      o
+      +
+      r
+      +
+      l
+      +
+      d
+      +
+      x
+    )
 
 console.log(f(), 'Hello, world!');
