dcaf3e46da4f8e0c81b91d5a4dc56bff0f4b1a89
taejin <taejin5314@gmail.com>
2021-07-04 11:39:29 -0400

    Update eureka.js

--- eureka.js
+++ eureka.js
@@ -11,10 +11,10 @@ function sumDigPow(a, b) {
   return arr;
 }
 
-console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
-console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
-console.log(sumDigPow(10, 100), [89])
-console.log(sumDigPow(90, 100), [])
-console.log(sumDigPow(90, 150), [135])
-console.log(sumDigPow(50, 150), [89, 135])
-console.log(sumDigPow(10, 150), [89, 135])
+console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
+console.log(sumDigPow(10, 100), [89]);
+console.log(sumDigPow(90, 100), []);
+console.log(sumDigPow(90, 150), [135]);
+console.log(sumDigPow(50, 150), [89, 135]);
+console.log(sumDigPow(10, 150), [89, 135]);
